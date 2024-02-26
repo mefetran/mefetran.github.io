d908ea663948078c8fafb5c2bbb48637d274b3a2
mefet <lidusikkajgo@gmail.com>
2024-02-26 22:08:00 +0300

    courswork additions done

--- coursework/game.js
+++ coursework/game.js
@@ -8,6 +8,7 @@ function game() {
     const changeColorButton = document.getElementById('changeColor');
     const clicsBlock = document.getElementById('clics');
     const scoresBlock = document.getElementById('score');
+    const remainsBlock = document.getElementById('remains');
     const stick = document.getElementById('stick');
     const timerBlock = document.getElementById('timer');
     let seconds = 0;
@@ -133,19 +134,6 @@ function game() {
         location.href = 'gameover.html';
     }
 
-    function changeColor(rings, rectangles) {
-        for (let i = colors.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [colors[i], colors[j]] = [colors[j], colors[i]];
-        }
-        rings.forEach((element, index) => {
-            element.style.borderColor = colors[index % colors.length];
-        });
-        rectangles.forEach((element, index) => {
-            element.style.backgroundColor = colors[index % colors.length];
-        })
-    }
-
     function changeColor(rings, rectangles, removedRings) {
         for (let i = colors.length - 1; i > 0; i--) {
             const j = Math.floor(Math.random() * (i + 1));
@@ -186,6 +174,8 @@ function game() {
         }
         changeColor(rings, rectangles, removedRings);
         stick.style.height = (rings.length * 4.16) + '%';
+        let remainingRings = rings.length;
+        remainsBlock.innerText = `Осталось: ${remainingRings}`;
         let order = rings.length - 1;
         let game = true;
         for (let i = 0; i < rings.length; i++) {
@@ -196,6 +186,8 @@ function game() {
                     clics++;
                     clicsBlock.innerText = `Клики: ${clics}`;
                     order--;
+                    remainingRings--;
+                    remainsBlock.innerText = `Осталось: ${remainingRings}`;
                     rings[i].remove();
                     let rectangle = document.createElement('div');
                     rectangle.classList.add(rectClasses[i]);
@@ -232,6 +224,76 @@ function game() {
     }
 
     function medium() {
+        function collision(ring, pyramidBlock) {
+            const { left: ringLeft, top: ringTop } = ring.getBoundingClientRect();
+            const { left: blockLeft, top: blockTop } = pyramidBlock.getBoundingClientRect();
+
+            return ringLeft >= blockLeft && ringLeft <= blockLeft + pyramidBlock.offsetWidth && ringTop >= blockTop && ringTop <= blockTop + pyramidBlock.offsetHeight;
+        }
+
+        function checkCollisions(ring, pyramidBlock) {
+            if (collision(ring, pyramidBlock)) {
+                removedRings.push(ring);
+                ring.style.display = 'none';
+                let rectangle = document.createElement('div');
+                rectangle.classList.add(rectClasses[rings.indexOf(ring)]);
+                rectangle.style.backgroundColor = ring.style.borderColor;
+                pyramidBlock.appendChild(rectangle);
+                rectangles.push(rectangle);
+                if (ringClasses[order] === ring.classList[0]) {
+                    score += 15 * difficultyCoefficient;
+                    scoresBlock.innerText = `Очки: ${score}`;
+                    clics++;
+                    clicsBlock.innerText = `Клики: ${clics}`;
+                    order--;
+                    remainingRings--;
+                    remainsBlock.innerText = `Осталось: ${remainingRings}`;
+                } else {
+
+                    score -= 30 * difficultyCoefficient;
+                    scoresBlock.innerText = `Очки: ${score}`;
+
+                    clics++;
+                    clicsBlock.innerText = `Клики: ${clics}`;
+                }
+            }
+        }
+
+        function mouseDown(e) {
+            const ring = e.target;
+            const ringArea = ring.parentElement;
+
+            const { clientX: mouseLeft, clientY: mouseTop } = e;
+            const { left: ringLeft, top: ringTop } = ring.getBoundingClientRect();
+            const { left: areaLeft, top: areaTop } = ringArea.getBoundingClientRect();
+
+            const shiftX = mouseLeft - ringLeft + areaLeft;
+            const shiftY = mouseTop - ringTop + areaTop;
+
+            previusZIndex = ring.style.zIndex;
+            ring.style.zIndex = 20;
+            ring.style.cursor = 'grabbing';
+
+            function moveAt(x, y) {
+                ring.style.left = x + 'px';
+                ring.style.top = y + 'px';
+            }
+
+            function mouseMove(e) {
+                moveAt(e.pageX - shiftX, e.pageY - shiftY);
+            }
+
+            function mouseUp() {
+                ring.style.zIndex = previusZIndex;
+                ring.style.cursor = 'grab';
+                ring.removeEventListener('mouseup', mouseUp);
+                document.removeEventListener('mousemove', mouseMove);
+                checkCollisions(ring, pyramidBlock);
+            }
+
+            ring.addEventListener('mouseup', mouseUp);
+            document.addEventListener('mousemove', mouseMove);
+        }
         document.getElementById('difficulty').innerText = 'Сложность: средняя';
         difficultyCoefficient = 2;
         const rings = [];
@@ -246,50 +308,47 @@ function game() {
             for (let i = 0; i < removedRings.length; i++) {
                 windowBlock.appendChild(removedRings[i]);
                 removedRings[i].style.display = 'block';
+                removedRings[i].classList.add('moveable');
+                removedRings[i].removeEventListener('mousedown', mouseDown);
                 moveRing(removedRings[i]);
             }
-            removedRings.length = 0;
+            setTimeout(() => {
+                for (let i = 0; i < removedRings.length; i++) {
+                    removedRings[i].classList.remove('moveable');
+                    removedRings[i].addEventListener('mousedown', mouseDown)
+                }
+                removedRings.length = 0;
+            }, 2000)
             order = rings.length - 1;
+            remainingRings = rings.length;
+            remainsBlock.innerText = `Осталось: ${remainingRings}`;
             clics++;
-            if (score > 0) {
-                score -= 15;
-                scoresBlock.innerText = `Очки: ${score}`;
-            }
+            score -= 50;
+            scoresBlock.innerText = `Очки: ${score}`;
+
         })
         for (let i = 0; i < getRandomInt(6, 10); i++) {
             rings.push(newRing(ringClasses[i]));
         }
         changeColor(rings, rectangles, removedRings);
         stick.style.height = (rings.length * 4.16) + '%';
+        let remainingRings = rings.length;
+        remainsBlock.innerText = `Осталось: ${remainingRings}`;
         changeColorButton.addEventListener('click', () => {
             changeColor(rings, rectangles, removedRings);
         })
-        let order = rings.length - 1;
-        for (let i = 0; i < rings.length; i++) {
-            rings[i].addEventListener('click', () => {
-                removedRings.push(rings[i]);
-                rings[i].style.display = 'none';
-                let rectangle = document.createElement('div');
-                rectangle.classList.add(rectClasses[i]);
-                rectangle.style.backgroundColor = rings[i].style.borderColor;
-                pyramidBlock.appendChild(rectangle);
-                rectangles.push(rectangle);
-                if (ringClasses[order] === rings[i].classList[0]) {
-                    score += 15 * difficultyCoefficient;
-                    scoresBlock.innerText = `Очки: ${score}`;
-                    clics++;
-                    clicsBlock.innerText = `Клики: ${clics}`;
-                    order--;
-                } else {
-
-                    score -= 30 * difficultyCoefficient;
-                    scoresBlock.innerText = `Очки: ${score}`;
-
-                    clics++;
-                    clicsBlock.innerText = `Клики: ${clics}`;
-                }
+        setTimeout(() => {
+            rings.forEach((element) => {
+                element.classList.remove('moveable');
+                element.addEventListener('mousedown', mouseDown)
+                element.ondragstart = () => false;
+                element.onselectstart = () => false;
+                element.style.cursor = 'grab';
             })
-        }
+            windowBlock.style.overflow = 'visible';
+        }, 50)
+
+        let order = rings.length - 1;
 
         let gameProcess = setInterval(() => {
             if (game) {
@@ -298,6 +357,7 @@ function game() {
                     rectangles.forEach(rectangle => {
                         rectangle.style.display = 'none';
                     })
+                    removedRings.length = 0;
                     hard();
                     clearInterval(gameProcess);
                 }
@@ -311,6 +371,7 @@ function game() {
     }
 
     function hard() {
+        windowBlock.style.overflow = 'hidden';
         document.getElementById('difficulty').innerText = 'Сложность: сложная';
         difficultyCoefficient = 3;
         const rings = [];
@@ -329,6 +390,8 @@ function game() {
             }
             removedRings.length = 0;
             order = rings.length - 1;
+            remainingRings = rings.length;
+            remainsBlock.innerText = `Осталось: ${remainingRings}`;
             clics++;
             if (score > 0) {
                 score -= 15;
@@ -340,13 +403,15 @@ function game() {
         }
         changeColor(rings, rectangles, removedRings);
         stick.style.height = (rings.length * 4.16) + '%';
+        let remainingRings = rings.length;
+        remainsBlock.innerText = `Осталось: ${remainingRings}`;
         changeColorButton.addEventListener('click', () => {
             changeColor(rings, rectangles, removedRings);
         })
         let order = rings.length - 1;
         let game = true;
         for (let i = 0; i < rings.length; i++) {
-            rings[i].addEventListener('click', () => {
+            rings[i].addEventListener('dblclick', () => {
                 removedRings.push(rings[i]);
                 rings[i].style.display = 'none';
                 let rectangle = document.createElement('div');
@@ -360,21 +425,20 @@ function game() {
                     clics++;
                     clicsBlock.innerText = `Клики: ${clics}`;
                     order--;
+                    remainingRings--;
+                    remainsBlock.innerText = `Осталось: ${remainingRings}`;
                 } else {
-
                     score -= 30 * difficultyCoefficient;
                     scoresBlock.innerText = `Очки: ${score}`;
-
                     clics++;
                     clicsBlock.innerText = `Клики: ${clics}`;
                 }
             })
         }
 
-
         let disappear = setInterval(() => {
             rings[Math.floor(Math.random() * rings.length)].style.display = 'none';
-        }, getRandomInt(2000, 4000));
+        }, getRandomInt(4000, 6000));
         let appear = setInterval(() => {
             const random = Math.floor(Math.random() * rings.length);
             let isInRemovedRings = false;
