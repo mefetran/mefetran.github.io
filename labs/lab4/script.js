78f283bcb0f45b85677611b3951f51e14da4aa43
mefet <lidusikkajgo@gmail.com>
2024-02-26 12:25:22 +0300

    added animation to lab5

--- labs/lab4/script.js
+++ labs/lab4/script.js
@@ -4,45 +4,45 @@ function start() {
     'Nota bene',
     'Nulla calamitas sola',
     'Per aspera ad astra',
-  ]
+  ];
 
   const array2 = [
     'Привычка - вторая натура',
     'Заметьте хорошо!',
     'Беда не приходит одна',
     'Через тернии к звёздам',
-  ]
+  ];
 
-  const usedNumbers = []
+  const usedNumbers = [];
 
-  const list = document.getElementById('list')
-  const add = document.getElementById('add')
-  const repaint = document.getElementById('repaint')
+  const list = document.getElementById('list');
+  const add = document.getElementById('add');
+  const repaint = document.getElementById('repaint');
 
   const getNumberOfNextElement = () => {
-    if (usedNumbers.length === array1.length) return null
+    if (usedNumbers.length === array1.length) return null;
 
-    const number = Math.floor(Math.random() * array1.length)
+    const number = Math.floor(Math.random() * array1.length);
 
-    if (usedNumbers.includes(number)) return getNumberOfNextElement()
+    if (usedNumbers.includes(number)) return getNumberOfNextElement();
 
-    usedNumbers.push(number)
-    return number
-  }
+    usedNumbers.push(number);
+    return number;
+  };
 
   add.addEventListener('click', () => {
-    const number = getNumberOfNextElement()
-    if (number === null) return alert('Фразы закончились')
+    const number = getNumberOfNextElement();
+    if (number === null) return alert('Фразы закончились');
     list.innerHTML += `
     <li class="class${usedNumbers.length % 2 ? '2' : '1'}">
         ${array1[number]}
         <ul><li>${array2[number]}</li></ul>
-    </li>`
-  })
+    </li>`;
+  });
 
   repaint.addEventListener('click', () => {
-      list.classList.toggle('repaint')
+      list.classList.toggle('repaint');
   })
 }
 
-start()
+start();
--- labs/lab5/script.js
+++ labs/lab5/script.js
@@ -1,8 +1,8 @@
 function start() {
-  const PERMITTED_DEVIATION = 10
-  const PER_ROTATE = 5
+  const tolerance = 10;
+  const rotateDeg = 5;
 
-  const keyElem = document.getElementById('ball')
+  const keyElem = document.getElementById('ball');
 
   const puzzles = new Map([
     [keyElem, { x: 0, y: 0 }],
@@ -12,93 +12,95 @@ function start() {
     [document.getElementById('chest'), { x: -24, y: -25 }],
     [document.getElementById('croup'), { x: 34, y: -25 }],
     [document.getElementById('tail'), { x: 73, y: -21 }],
-  ])
+  ]);
 
   for (const puzzle of puzzles.keys()) {
-    puzzle.firstElementChild.style.rotate = `${Math.floor(Math.random() * 360)}deg`
-    puzzle.addEventListener('mousedown', mouseDown)
-    puzzle.ondragstart = () => false
-    puzzle.onselectstart = () => false
+    puzzle.firstElementChild.style.rotate = `${Math.floor(Math.random() * 360)}deg`;
+    puzzle.addEventListener('mousedown', mouseDown);
+    puzzle.ondragstart = () => false;
+    puzzle.onselectstart = () => false;
   }
 
   function check() {
-    const { left: keyLeft, top: keyTop } = keyElem.getBoundingClientRect()
+    const { left: keyLeft, top: keyTop } = keyElem.getBoundingClientRect();
 
     for (const [puzzle, { x, y }] of puzzles) {
-      const puzzleImage = puzzle.firstElementChild
-      const computedStyles = window.getComputedStyle(puzzleImage)
+      const puzzleImage = puzzle.firstElementChild;
+      const computedStyles = window.getComputedStyle(puzzleImage);
       let rotate =
         computedStyles.rotate !== 'none'
           ? Number(computedStyles.rotate.slice(0, -3))
-          : 0
-      
-      rotate = Math.abs(rotate) % 360
-      
-      if (rotate > 0 + PERMITTED_DEVIATION && rotate < 360 - PERMITTED_DEVIATION) {
-        return false
+          : 0;
+
+      rotate = Math.abs(rotate) % 360;
+
+      if (rotate > 0 + tolerance && rotate < 360 - tolerance) {
+        return false;
       }
 
       const { left: puzzleLeft, top: puzzleTop } =
-        puzzle.getBoundingClientRect()
+        puzzle.getBoundingClientRect();
 
       if (
-        Math.abs(puzzleLeft - keyLeft - x) > PERMITTED_DEVIATION ||
-        Math.abs(puzzleTop - keyTop - y) > PERMITTED_DEVIATION
+        Math.abs(puzzleLeft - keyLeft - x) > tolerance ||
+        Math.abs(puzzleTop - keyTop - y) > tolerance
       )
-        return
+        return;
     }
 
-    alert('Пазл успешно собран!')
-    // TODO тут надо реализовать анимацию, простейший вариант - добавить какой-нибудь класс на элемент, который хочешь анимировать
+    alert('Пазл успешно собран!');
+
+    // simple animation
+    keyElem.classList.add('anim');
   }
 
   function mouseDown(e) {
-    const puzzle = e.target
-    const puzzleImage = puzzle.firstElementChild
-    const puzzleArea = puzzle.parentElement
+    const puzzle = e.target;
+    const puzzleImage = puzzle.firstElementChild;
+    const puzzleArea = puzzle.parentElement;
 
-    const { clientX: mouseLeft, clientY: mouseTop } = e
-    const { left: puzzleLeft, top: puzzleTop } = puzzle.getBoundingClientRect()
-    const { left: areaLeft, top: areaTop } = puzzleArea.getBoundingClientRect()
+    const { clientX: mouseLeft, clientY: mouseTop } = e;
+    const { left: puzzleLeft, top: puzzleTop } = puzzle.getBoundingClientRect();
+    const { left: areaLeft, top: areaTop } = puzzleArea.getBoundingClientRect();
 
-    const shiftX = mouseLeft - puzzleLeft + areaLeft
-    const shiftY = mouseTop - puzzleTop + areaTop
+    const shiftX = mouseLeft - puzzleLeft + areaLeft;
+    const shiftY = mouseTop - puzzleTop + areaTop;
 
-    puzzle.style.zIndex = 10
-    puzzle.style.cursor = 'grabbing'
+    puzzle.style.zIndex = 10;
+    puzzle.style.cursor = 'grabbing';
 
     function moveAt(x, y) {
-      puzzle.style.left = x + 'px'
-      puzzle.style.top = y + 'px'
+      puzzle.style.left = x + 'px';
+      puzzle.style.top = y + 'px';
     }
 
     function mouseMove(e) {
-      moveAt(e.pageX - shiftX, e.pageY - shiftY)
+      moveAt(e.pageX - shiftX, e.pageY - shiftY);
     }
 
     function wheel(e) {
-      const computedStyles = window.getComputedStyle(puzzleImage)
+      const computedStyles = window.getComputedStyle(puzzleImage);
       const currentRotate =
         computedStyles.rotate !== 'none'
           ? Number(computedStyles.rotate.slice(0, -3))
-          : 0
-      puzzleImage.style.rotate = `${currentRotate + (e.deltaY < 0 ? -PER_ROTATE : PER_ROTATE)
-        }deg`
+          : 0;
+      puzzleImage.style.rotate = `${currentRotate + (e.deltaY < 0 ? -rotateDeg : rotateDeg)
+        }deg`;
     }
 
     function mouseUp() {
-      puzzle.style.zIndex = 5
-      puzzle.style.cursor = 'grab'
-      puzzle.removeEventListener('mouseup', mouseUp)
-      document.removeEventListener('wheel', wheel)
-      document.removeEventListener('mousemove', mouseMove)
-      check()
+      puzzle.style.zIndex = 5;
+      puzzle.style.cursor = 'grab';
+      puzzle.removeEventListener('mouseup', mouseUp);
+      document.removeEventListener('wheel', wheel);
+      document.removeEventListener('mousemove', mouseMove);
+      check();
     }
 
-    puzzle.addEventListener('mouseup', mouseUp)
-    document.addEventListener('wheel', wheel)
-    document.addEventListener('mousemove', mouseMove)
+    puzzle.addEventListener('mouseup', mouseUp);
+    document.addEventListener('wheel', wheel);
+    document.addEventListener('mousemove', mouseMove);
   }
 }
 
-start()
+start();
